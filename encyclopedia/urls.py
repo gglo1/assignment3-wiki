ac1061b29b431585934f064c393d8744208a4945
chezmarcbrown <marc@chezbrown.org>
2023-10-14 14:46:49 -0800

    feedback; see email for details

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
-    path("create", views.create, name="create"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("random", views.rand, name="rand")
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("edit/<str:title>/", views.edit, name="edit"),
+    path("random/", views.rand, name="rand")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import Markdown
 from django import forms
 import random
@@ -27,44 +27,37 @@ class EditForm(forms.Form): #create an edit page
 def entry(request, title): #switch to show specified page
     f = util.get_entry(title) #check if page exists
     if f is None:
-        form = SearchForm()
         content = "No matches were found" #shows the error page
-        return render(request, "encyclopedia/error.html", {"form": form, "content": content})
+        return render(request, "encyclopedia/error.html", {"content": content})
     else:
-        form = SearchForm()
         wikicontent = util.get_entry(title) 
         htmlcontent = markdowner.convert(wikicontent) #convert markdown data to html
-        return render(request, "encyclopedia/entry.html", {"title": title, "content": htmlcontent, "form": form})
+        return render(request, "encyclopedia/entry.html", {"title": title, "content": htmlcontent})
 
 
 def search(request): #display user's searched page or list of relevant results
-    if request.method == "POST": #active page search
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data.get("query")
-            present = False
-            for entry in util.list_entries():
-                if data.lower() == entry.lower(): #to make case insensitive
-                    wikicontent = util.get_entry(data)
-                    htmlcontent = markdowner.convert(wikicontent)
-                    present = True
-                    break
-            if present: #display if page is found
-                return render(request, "encyclopedia/entry.html", {"content": htmlcontent, "form": form, "title": data})
-            else: #otherwise check substrings
-                listE = []
-                for entry in util.list_entries():
-                    index = entry.lower().find(data.lower()) #to make case insensitive for search
-                    if index  != -1:
-                        listE.append(entry)
-                if len(listE) == 0: #display error page if no relevant content is found
-                    form = SearchForm()
-                    content = "The page you requested was not found"
-                    return render(request, "encyclopedia/error.html", {'form': form, "content": content})
-                else: #list pages with relevant substrings found
-                    return render(request, "encyclopedia/index.html", {"entries": listE, "form": form})
-    else: #otherwise display error page
-        return render(request, "encyclopedia/error.html", {'form': form, "content": content})
+    data = request.GET.get("query")
+    present = False
+    for entry in util.list_entries():
+        if data.lower() == entry.lower(): #to make case insensitive
+            # wikicontent = util.get_entry(data)
+            # htmlcontent = markdowner.convert(wikicontent)
+            present = True
+            break
+    if present: #display if page is found
+        return redirect('encyclopedia:entry', title=data)
+        #eturn render(request, "encyclopedia/entry.html", {"content": htmlcontent, "title": data})
+    else: #otherwise check substrings
+        listE = []
+        for entry in util.list_entries():
+            index = entry.lower().find(data.lower()) #to make case insensitive for search
+            if index  != -1:
+                listE.append(entry)
+        if len(listE) == 0: #display error page if no relevant content is found
+            content = "The page you requested was not found"
+            return render(request, "encyclopedia/error.html", {"content": content})
+        else: #list pages with relevant substrings found
+            return render(request, "encyclopedia/index.html", {"entries": listE})
 
 
 def create(request): #create a new page
@@ -101,23 +94,25 @@ def edit(request, title): #edit a specified page
             title = editform.cleaned_data.get("title")
             body = editform.cleaned_data.get("content")
             util.save_entry(title, body) #saves new content
-            form = SearchForm()
-            htmlcontent = markdowner.convert(body)
-            return render(request, "encyclopedia/entry.html", {
-                "title": title, "content": htmlcontent, "form": form
-            })
+            redirect('entry', title)
+            # form = SearchForm()
+            # htmlcontent = markdowner.convert(body)
+            # return render(request, "encyclopedia/entry.html", {
+            #     "title": title, "content": htmlcontent, "form": form
+            # })
     else:
-        form = SearchForm()
-        editform = EditForm({"title": title, "body": util.get_entry(title)})
-        return render(request, "encyclopedia/edit.html", {"form": form, "editform": editform})
+        #form = SearchForm()
+        editform = EditForm({"title": title, "content": util.get_entry(title)})
+    return render(request, "encyclopedia/edit.html", {"editform": editform})
 
 
 def rand(request): #displays random page from encylcopedia
     entries = util.list_entries()
     num = len(entries)
-    entry = random.randint(0, num-1) #generates a random number for entry selection
-    title = entries[entry]
-    wikicontent = util.get_entry(title)
-    htmlcontent = markdowner.convert(wikicontent)
-    form = SearchForm()
-    return render(request, "encyclopedia/rand.html", {"form": form, "title": title, "content": htmlcontent})
+    i = random.randint(0, num-1) #generates a random number for entry selection
+    title = entries[i]
+    return redirect('encyclopedia:entry', title=title)
+    # wikicontent = util.get_entry(title)
+    # htmlcontent = markdowner.convert(wikicontent)
+    # form = SearchForm()
+    # return render(request, "encyclopedia/rand.html", {"form": form, "title": title, "content": htmlcontent})
